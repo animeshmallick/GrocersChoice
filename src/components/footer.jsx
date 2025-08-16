51114886d635536d0e99fc7c066ebb6e40aaeea6
= <animesh7995@gmail.com>
2025-08-16 20:11:52 +0530

    add version to footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,8 +3,9 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer className="bg-emerald-700 text-white py-6 mt-10 text-center text-sm">
+        <footer className="bg-emerald-700 text-white py-4 mt-10 text-center text-sm">
             <p>&copy; {new Date().getFullYear()} BSquare SuperMart. All rights reserved.</p>
+            <p>Version: 1.1.1</p>
         </footer>
     );
 };
