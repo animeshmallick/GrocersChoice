c58df38dcb7a143658a32a22ab7930acda53463f
= <animesh7995@gmail.com>
2025-08-16 20:44:42 +0530

    show version only in non prod env

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,10 +2,11 @@
 import React from "react";
 
 const Footer = () => {
+    const isProdEnv = window.location.hostname.indexOf('www.') >= 0;
     return (
         <footer className="bg-emerald-700 text-white py-4 mt-10 text-center text-sm">
             <p>&copy; {new Date().getFullYear()} BSquare SuperMart. All rights reserved.</p>
-            <p>Version: 1.1.1</p>
+            {!isProdEnv && <p>Version: 1.1.1</p>}
         </footer>
     );
 };
